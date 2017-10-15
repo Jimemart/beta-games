8aa09504d18099c6a41bc4976d1c4a32373068e3
Jimemart <jimena.mg@hotmail.com>
2017-10-15 19:23:43 +0200

    Compilado

--- client/src/app/pick-plat/pick-plat.component.ts
+++ client/src/app/pick-plat/pick-plat.component.ts
@@ -8,6 +8,7 @@ import { AuthService } from '../services/auth.service'
 })
 export class PickPlatComponent implements OnInit {
   myPlats=[]
+  xBox = 'https://lh3.googleusercontent.com/YqlXGn_KbC5zxxEDr12vsYWjcMpZokEmGZ76opYh4VmRwa2FCP6fHYzFcad5-xj4C677=w300'
   constructor(public auth:AuthService) { }
 
   ngOnInit() {
